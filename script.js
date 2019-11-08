eca041a5c879c0139070f88e5831e063fba97441
rlinares1 <44908875+rlinares1@users.noreply.github.com>
2019-11-08 10:50:45 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// Row 1
+var a = document.getElementById("one");
+var b = document.getElementById("two");
+var c = document.getElementById("three");
+
+// Row 2
+var d = document.getElementById("four");
+var e = document.getElementById("five");
+var f = document.getElementById("six");
+
+//Row 3
+var g = document.getElementById("seven");
+var h = document.getElementById("eight");
+var i = document.getElementById("nine");
+
+//Row 4
+var j = document.getElementById("ten");
+var k = document.getElementById("eleven");
+var l = document.getElementById("twelve");
+
+//Row 5
+var m = document.getElementById("thirteen");
+var n = document.getElementById("fourteen");
+var o = document.getElementById("fifteen");
+
+function soundClick(elem){
+	document.getElementById("sfxhagg").play();
+}
+
+function buttonClicked(elem){
+	document.getElementById("sfxpop").play();
+}
